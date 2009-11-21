4c91404cefb58bd1bb6174e03bda7cd6694795f7
Rick E <codeblock@eighthbit.net>
2009-11-21 12:46:34 -0500

    Initial set of models for contest/.
    Also fixed:
      - settings.py didn't import os, but needed it for the relpath() hack.
      - made it so our real database isn't open to the world ;)
      - Default username/password for the public database is admin/admin.

--- contest/models.py
+++ contest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Status(models.Model):
+   # Status of the contest.
+   name = models.CharField('Status', max_length = 100)
+
+   def __unicode__(self):
+      return self.name
+
+class Contest(models.Model):
+   title = models.CharField('Contest Title', max_length = 200)
+   starts = models.DateTimeField('Start Date', auto_now = True, auto_now_add = True)
+   ends = models.DateTimeField('End Date', auto_now = True, auto_now_add = True)
+   status = models.ForeignKey(Status)
+   summary = models.TextField('Contest Details')
+
+   def __unicode__(self):
+      return self.title
+
