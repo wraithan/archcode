98aae7c872287b65fc50e5a0a8c547643307ef6a
Wraithan <XWraithanX@gmail.com>
2009-11-23 00:41:07 -0800

    Changed contest to challenge, as has been previously discussed in #archlinux-code. These are to be fun challenges instead of contests for best solution (as different languages or approachs can be better in different ways)

--- challenge/models.py
+++ challenge/models.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from django.db import models
 
 class Status(models.Model):
-   # Status of the contest.
+   # Status of the challenge.
    name = models.CharField('Status', max_length = 100)
 
    class Meta:
@@ -11,12 +11,12 @@ class Status(models.Model):
    def __unicode__(self):
       return self.name
 
-class Contest(models.Model):
-   title = models.CharField('Contest Title', max_length = 200)
+class Challenge(models.Model):
+   title = models.CharField('Challenge Title', max_length = 200)
    starts = models.DateTimeField('Start Date', default = datetime.now)
    ends = models.DateTimeField('End Date', default = lambda: datetime.now() + timedelta(days=7))
    status = models.ForeignKey(Status)
-   summary = models.TextField('Contest Details')
+   summary = models.TextField('Challenge Details')
 
    def __unicode__(self):
       return self.title
--- contest/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from archcode.contest.models import *
-admin.site.register(Status)
-admin.site.register(Contest)
