b435804add4368821e225e71a7b2fdc6ca4b79f3
Wraithan <XWraithanX@gmail.com>
2009-11-25 03:20:19 -0800

    Added get_absolute_url to Challenge, updated index.html to take advantage of it.

--- challenge/models.py
+++ challenge/models.py
@@ -8,12 +8,15 @@ CHALLENGE_STATUS_CHOICES = (
 )
 
 class Challenge(models.Model):
-   title = models.CharField('Challenge Title', max_length = 200)
-   starts = models.DateTimeField('Start Date', default = datetime.now)
-   ends = models.DateTimeField('End Date', default = lambda: datetime.now() + timedelta(days=7))
-   status = models.CharField('Challenge Status', max_length = 2, choices=CHALLENGE_STATUS_CHOICES)
-   summary = models.TextField('Challenge Details')
+    title = models.CharField('Challenge Title', max_length = 200)
+    starts = models.DateTimeField('Start Date', default = datetime.now)
+    ends = models.DateTimeField('End Date', default = lambda: datetime.now() + timedelta(days=7))
+    status = models.CharField('Challenge Status', max_length = 2, choices=CHALLENGE_STATUS_CHOICES)
+    summary = models.TextField('Challenge Details')
 
-   def __unicode__(self):
-      return self.title
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return "/challenge/%i/" % self.id
 
