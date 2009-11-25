6827f8c27a43b35ee79bebf6ee2e4f9181fb02c6
Wraithan <XWraithanX@gmail.com>
2009-11-25 03:01:01 -0800

    Accidently reverted the wrong change earlier, readding css support.

--- urls.py
+++ urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+import os
 admin.autodiscover()
-
+relpath = lambda *x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+ 
 urlpatterns = patterns('',
     (r'^$', 'archcode.challenge.views.index'),
     (r'^challenge/(?P<challenge_id>\d+)/$', 'archcode.challenge.views.details'),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': relpath('challenge/media/')}),
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+ 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
-)
+) 
+
