1488196750e4222288b4ed13965793e34bf3750f
Wraithan <XWraithanX@gmail.com>
2009-11-25 14:22:27 -0800

    Found that I didn't need to wrap filters with try/except, just gets. Cleaned up code.

--- challenge/views.py
+++ challenge/views.py
@@ -4,14 +4,8 @@ from archcode.challenge.models import Challenge
 from datetime import datetime
 
 def index(request):
-    try:
-        current_challenges = Challenge.objects.exclude(ends__lte=datetime.now).filter(starts__lte=datetime.now)
-    except Challenge.DoesNotExist:
-        current_challenges = None
-    try:
-        past_challenges = Challenge.objects.filter(ends__lte=datetime.now)
-    except Challenge.DoesNotExist:
-        past_challenges = None
+    current_challenges = Challenge.objects.exclude(ends__lte=datetime.now).filter(starts__lte=datetime.now)
+    past_challenges = Challenge.objects.filter(ends__lte=datetime.now)
     return render_to_response('index.html', {'current_challenges': current_challenges,'past_challenges': past_challenges})
 
 def details(request, challenge_id):
