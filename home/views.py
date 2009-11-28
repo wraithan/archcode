dd4c68d436ed3bc18517e1f6053921d41bd253be
Chris McDonald <XWraithanX@gmail.com>
2009-11-28 09:05:08 -0800

    Changed home/views.py to remove the test messages.

--- home/views.py
+++ home/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render_to_response, get_object_or_404
 def index(request):
     title = "Welcome"
 
-    messages = [ "This is a test message.", "This is another test message." ]
     return render_to_response('home/index.html', {
         'title':                title,
-        'messages':              messages,
         })
