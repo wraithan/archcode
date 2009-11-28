bc196edabf08735c908158ea5112c1e1eb1fedc2
Chris McDonald <XWraithanX@gmail.com>
2009-11-28 09:48:24 -0800

    Fixed a small problem in Xilon's patch to challenge/urls.py (index was set to $ which matched everything, instead of ^$ which only matches root)

--- challenge/urls.py
+++ challenge/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('archcode.challenge.views',
-    (r'$', 'index', {}, 'challenge-index'),
+    (r'^$', 'index', {}, 'challenge-index'),
     (r'(?P<challenge_id>\d+)/$', 'details', {}, 'challenge-details'),
 )
