94bd6831be6e8b8eb8f33dd63871ed9912a5e98f
Michael Fiano <axedcode@gmail.com>
2009-11-28 05:11:25 -0500

    Changed design to work with site structure; Challenge view now includes design template.

--- challenge/views.py
+++ challenge/views.py
@@ -4,9 +4,14 @@ from archcode.challenge.models import Challenge
 from datetime import datetime
 
 def index(request):
+    title = "Challenges"
     current_challenges = Challenge.objects.exclude(ends__lte=datetime.now).filter(starts__lte=datetime.now)
     past_challenges = Challenge.objects.filter(ends__lte=datetime.now)
-    return render_to_response('challenge/index.html', {'current_challenges': current_challenges,'past_challenges': past_challenges})
+    return render_to_response('challenge/index.html', {
+        'title':                title,
+        'current_challenges':   current_challenges,
+        'past_challenges':      past_challenges
+        })
 
 def details(request, challenge_id):
     challenge = get_object_or_404(Challenge, pk=challenge_id)
