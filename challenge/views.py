dfe06799107fe19cfcf9f59551ee5fdd9d56b03c
Michael Fiano <axedcode@gmail.com>
2009-11-28 17:54:03 -0500

    More positioning stuff.

--- challenge/views.py
+++ challenge/views.py
@@ -4,14 +4,14 @@ from archcode.challenge.models import Challenge
 from datetime import datetime
 
 title = "Challenges"
-messages = False
+message = False
 
 def index(request):
     current_challenges = Challenge.objects.exclude(ends__lte=datetime.now).filter(starts__lte=datetime.now)
     past_challenges = Challenge.objects.filter(ends__lte=datetime.now)
     return render_to_response('challenge/index.html', {
         'title':                title,
-        'messages':             messages,
+        'message':              message,
         'current_challenges':   current_challenges,
         'past_challenges':      past_challenges
         })
