3b7631fa7aec9349c83ca3192a62718b1d777d85
Michael Fiano <axedcode@gmail.com>
2009-11-28 11:20:01 -0500

    Added basic support for multiple alert messages.

--- home/views.py
+++ home/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     title = "Welcome"
-    message = "This is a message."
+
+    messages = [ "This is a test message.", "This is another test message." ]
     return render_to_response('home/index.html', {
         'title':                title,
-        'message':              message,
+        'messages':              messages,
         })
