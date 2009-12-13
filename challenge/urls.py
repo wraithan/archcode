d695ae34b852019dfc4cf3cbb2df087a339bc86d
Chris McDonald <XWraithanX@gmail.com>
2009-12-13 13:38:27 -0800

    Now you can view a list of solutions and even view the details!

--- challenge/urls.py
+++ challenge/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('archcode.challenge.views',
-    url(r'^$', 'index', {}, 'challenge-index'),
-    url(r'(?P<challenge_id>\d+)/$', 'details', {}, 'challenge-details'),
-    url(r'submit/solution/$', 'submit_solution', name='challenge-submit-solution'),
+    url(r'^$', 'challenge_list', name='challenge-list'),
+    url(r'^(?P<challenge_id>\d+)/$', 'challenge_details', name='challenge-details'),
+
+    url(r'solution/list/$', 'solution_list', name='challenge-solution-list'),
+    url(r'solution/(?P<solution_id>\d+)/$', 'solution_details', name='challenge-solution-details'),
+    url(r'solution/submit/$', 'solution_submit', name='challenge-solution-submit'),
 )
--- challenge/views.py
+++ challenge/views.py
@@ -1,38 +1,61 @@
-# Create your views here.
-from archcode.challenge.models import Challenge
-from archcode.challenge.forms import SolutionForm
 from datetime import datetime
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 
-title = "Challenges"
+from archcode.challenge.models import Challenge, Solution
+from archcode.challenge.forms import SolutionForm
 
-def index(request):
+
+
+def challenge_list(request):
+    title = "Challenges"
     current_challenges = Challenge.objects.exclude(ends__lte=datetime.now).filter(starts__lte=datetime.now)
     past_challenges = Challenge.objects.filter(ends__lte=datetime.now)
-    return render_to_response('challenge/index.html', {
-        'title':                title,
-        'current_challenges':   current_challenges,
-        'past_challenges':      past_challenges
+    return render_to_response('challenge/challenge_list.html', {
+        'title': title,
+        'current_challenges': current_challenges,
+        'past_challenges': past_challenges
         })
 
-def details(request, challenge_id):
+def challenge_details(request, challenge_id):
+    title = "Challenge Details"
     challenge = get_object_or_404(Challenge, pk=challenge_id)
-    return render_to_response('challenge/details.html', {
+    return render_to_response('challenge/challenge_details.html', {
         'title': title,
         'challenge': challenge })
 
+def solution_list(request):
+    title = 'Solutions'
+    solutions = Solution.objects.all()
+    return render_to_response('challenge/solution_list.html', {
+        'title': title,
+        'solutions': solutions,
+        })
+
+def solution_details(request, solution_id):
+    title = "Solution Details"
+    solution = get_object_or_404(Solution, pk=solution_id)
+    return render_to_response('challenge/solution_details.html', {
+        'title': title,
+        'solution': solution })
+
 @login_required
-def submit_solution(request):
+def solution_submit(request):
+    title = 'Submit a Solution'
     if request.method == 'POST':
         form = SolutionForm(request.POST)
         if form.is_valid():
             object = form.save(commit=False)
-            object.user = request.user# User.objects.filter(id=request.user.id).objects
+            object.user = request.user
             object.save()
-            return HttpResponseRedirect('/challenge/submit/solution/')
+            return HttpResponseRedirect(reverse('challenge-solution-submit'))
     else:
         form = SolutionForm()
-    return render_to_response('challenge/submit_solution.html', {'form':form})
+    return render_to_response('challenge/solution_submit.html', {
+        'title': title,
+        'form': form})
+
